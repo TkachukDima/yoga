e7dfe2fda01e4fbbc187b27c45f4c1ecec7a35a5
Dima <tkachuk.dima97@gmail.com>
2019-02-19 14:16:14 +0200

    yoga timer

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -36,7 +36,54 @@ window.addEventListener('DOMContentLoaded', function() {
           }
         }
     }
-  })
+  });
+
+  // Timer
+
+  let deadLine = '2019-04-17';
+
+  function getTimeRemaining(endtime) {
+    let t = Date.parse(deadLine) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60 ),
+        minutes = Math.floor( (t/1000/60) % 60 ),
+        hours = Math.floor( (t/1000/60/60) );
+
+        return {
+          'total': t,
+          'hours': hours,
+          'minutes': minutes,
+          'seconds': seconds
+        }
+  }
+
+  function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds');
+            
+    let timeInterval = setInterval(updateClock, 1000);
+
+
+
+
+    function updateClock() {
+      let t = getTimeRemaining(endtime);
+      if(t.hours < 10) t.hours = "0" + t.hours;
+      hours.textContent = t.hours;
+      if(t.minutes < 10) t.minutes = "0" + t.minutes;
+      minutes.textContent = t.minutes;
+      if(t.seconds < 10) t.seconds = "0" + t.seconds;
+      seconds.textContent = t.seconds;
+
+      if( t <= 0 ) {
+        clearInterval(timeInterval);
+      }
+    }
+
+  }
+
+  setClock('timer', deadLine);
 
 
 });
